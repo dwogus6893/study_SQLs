69191e74244f65d10453c42cbdd46ebc1f82e36c
dwogus6893 <dwogus6893@naver.com>
2022-10-17 14:56:32 +0900

    leejaehyun

--- leejaehyun/w3schools/Employees_Order_total.sql
+++ leejaehyun/w3schools/Employees_Order_total.sql
@@ -1,10 +1,10 @@
 # 직원(Employees) EmployeeID가 7, 9인 종업원이 받는 주문(Orders) 총 수는 ?
 # Employees_Order_totals.sql
 
-SELECT EmployeeID 
-FROM Employees
-WHERE Orders IN (SELECT Orders
+SELECT *
+FROM Orders
+WHERE EmployeeID IN (SELECT EmployeeID
                  FROM Employees
-                 WHERE EmployeeID(7,9));
+                 WHERE EmployeeID IN (7,9));
 
 
