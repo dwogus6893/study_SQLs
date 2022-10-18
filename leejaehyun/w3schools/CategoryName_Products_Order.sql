0e5d0d9e05e4fdee2ae8fc96ae81437def6d6324
dwogus6893 <dwogus6893@naver.com>
2022-10-19 00:05:59 +0900

    2022-10-19

--- leejaehyun/w3schools/CategoryName_Products_Order.sql
+++ leejaehyun/w3schools/CategoryName_Products_Order.sql
@@ -1,17 +1,30 @@
--- + 7 : CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 주문  OrderID 내역과 총 갯수는 ?
+-- + 7 : CategoryName 가 Dairy Products, Seafood이고
+--Price 10.00이상 50.00이하인 주문  OrderID 내역과 총 갯수는 ?
+=========================================
 
+--WHERE은 조건문, 연결고리
+--CategoryName 가 Dairy Products, Seafood이고
+SELECT CategoryID
+FROM Categories
+WHERE CategoryName IN ('Dairy Products','Seafood');
 
+--Price 10.00이상 50.00이하인
+SELECT ProductID
+FROM Products
+WHERE Price BETWEEN 10 AND 50;
 
-SELECT OrderID 
-FROM Orders
-WHERE OrderID 
-IN(SELECT OrderID
-	FROM OrderDetails
-	WHERE ProductID 
-IN (SELECT ProductID
-    FROM Products
-    WHERE Price BETWEEN 10 AND 50 
-	AND CategoryID 
-IN (SELECT CategoryID
-    FROM Categories
-                    					                WHERE CategoryName IN('Dairy Products','Seafood'))));
+--주문  OrderID 내역과 총 갯수는 ?
+SELECT OrderID ,Count(OrderID)
+FROM OrdersDetails;
+
+=========================================
+
+SELECT OrderID, Count(OrderID)
+FROM OrdersDetails
+WEHRE ProductID IN (SELECT ProductID
+					FROM Products
+					WHERE Price BETWEEN 10 AND 50
+					AND CategoryId IN(
+					SELECT CategoryID
+					FROM Categories
+					WHERE CategoryName IN ('Dairy Products','Seafood')));
