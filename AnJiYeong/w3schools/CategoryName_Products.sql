4a7ed2bade2f2520f890cf2b326a5a4a629f1e96
AnJiYeong <memarcis2@naver.com>
2022-10-18 12:45:14 +0900

    UP

---
+++ AnJiYeong/w3schools/CategoryName_Products.sql
@@ -0,0 +1,8 @@
+-- CategoryName 가 Dairy Products, Seafood이고
+-- Price 10.00이상50.00이하인 합계와 평균은 ?
+SELECT SUM(Price)
+FROM Products
+WHERE CategoryID IN (SELECT CategoryID
+					FROM Categories
+                    WHERE CategoryName IN ('Dairy Products', 'Seafood')
+                    AND (Price BETWEEN 10.00 AND 50.00));
