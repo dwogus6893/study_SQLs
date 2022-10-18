7454abc473e4636b431b641a9cd9cf84c4df8a39
Ryuyeonjoo <fuswn87@gmail.com>
2022-10-18 15:28:00 +0900

    yeonjoo

--- yeonjoooryu/w3schools/Categories_Products_Count.sql
+++ yeonjoooryu/w3schools/Categories_Products_Count.sql
@@ -1,14 +1,14 @@
 --Categories별로 구성된 제품 갯수, 가격 평균, 최고값, 최저값 표시하세요.
-SELECT COUNT(ProductID),CategoryID, AVG(Price),MIN(Price),MAX(Price)
+SELECT COUNT(*), CategoryID, AVG(Price), MAX(Price), MIN(Price)
 FROM Products
 GROUP BY CategoryID;
 
---COUNT(ProductID)	CategoryID	AVG(Price)	MIN(Price)	MAX(Price)
---12	                1	    37.979167  	4.50	    263.50
---12	                2	    23.062500	10.00	    43.90
---13	                3	    25.160000	9.20	    81.00
---10	                4	    28.730000	2.50	    55.00
---7	                    5	    20.250000	7.00	    38.00
---6	                    6	    54.006667	7.45	    123.79
---5	                    7	    32.370000	10.00	    53.00
---12	                8	    20.682500	6.00	    62.50
+COUNT(*)	CategoryID	AVG(Price)	MAX(Price)	MIN(Price)
+12	1	37.979167	263.50	4.50
+12	2	23.062500	43.90	10.00
+13	3	25.160000	81.00	9.20
+10	4	28.730000	55.00	2.50
+7	5	20.250000	38.00	7.00
+6	6	54.006667	123.79	7.45
+5	7	32.370000	53.00	10.00
+12	8	20.682500	62.50	6.00
