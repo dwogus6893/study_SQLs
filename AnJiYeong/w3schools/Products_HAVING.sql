b1e3822f261506f0cdd6c85a74e6a5b59e5e4294
AnJiYeong <memarcis2@naver.com>
2022-10-19 10:20:52 +0900

    up

--- AnJiYeong/w3schools/Products_HAVING.sql
+++ AnJiYeong/w3schools/Products_HAVING.sql
@@ -1,5 +1,5 @@
 -- +1 Products 카테고리에 10 이상인 제품 있는 카테고리와 개수 표시?
-SELECT CategoryID, COUNT(*)
+SELECT CategoryID, COUNT(CategoryID)
 FROM Products
 GROUP BY CategoryID
 HAVING COUNT(CategoryID) >= 10;
