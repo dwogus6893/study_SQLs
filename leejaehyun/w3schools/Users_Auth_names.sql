9f0c25d115a8ec193983428f171474864cb3ca59
dwogus6893 <dwogus6893@naver.com>
2022-10-20 15:10:42 +0900

    2022-10-20

--- leejaehyun/w3schools/Users_Auth_names.sql
+++ leejaehyun/w3schools/Users_Auth_names.sql
@@ -1,5 +1,5 @@
-SELECT users.NAME, users.EMAIL,auths.UNIQUE_ID_AUTH_NAMES
-FROM users
-INNER JOIN auths
+SELECT users.NAME, users.EMAIL,auth_names.AUTH_NAME
+FROM (users INNER JOIN auths
 ON users.UNIQUE_ID = UNIQUE_ID_USERS
-AND users.Name IN ('TEDDY','Allen');
+AND users.Name IN ('TEDDY','Allen')) 
+INNER JOIN auth_names ON auths.UNIQUE_ID_AUTH_NAMES = AUTH_NAMEs.UNIQUE_ID;
